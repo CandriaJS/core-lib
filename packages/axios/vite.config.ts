520c2724657e1595208419414cab49369018d94b
wuliya <3369906077@qq.com>
2025-06-18 19:47:19 +0800

    build(axios): 使用 Vite 替代 tsup 构建 Axios

---
+++ packages/axios/vite.config.ts
@@ -0,0 +1,52 @@
+import fs from 'node:fs'
+import { builtinModules } from 'node:module'
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    target: 'node22',
+    lib: {
+      formats: [
+        'es'
+      ],
+      fileName: () => 'index.js',
+      entry: [
+        'src/index.ts'
+      ]
+    },
+    emptyOutDir: true,
+    outDir: 'dist',
+    rollupOptions: {
+      external: [
+        ...builtinModules,
+        ...builtinModules.map((mod) => `node:${mod}`),
+        'form-data'
+      ],
+      output: {
+        inlineDynamicImports: true
+      },
+      cache: false
+    },
+    minify: false,
+    commonjsOptions: {
+      include: [
+        /node_modules/
+      ],
+      transformMixedEsModules: true,
+      defaultIsModuleExports: true
+    }
+  },
+  /**
+   * 编译结束插件
+   */
+  plugins: [
+    {
+      name: 'axios-plugin',
+      closeBundle () {
+        fs.cpSync('./node_modules/axios/index.d.ts', './dist/index.d.ts')
+        console.log('构建axios成功!')
+      }
+    }
+  ]
+})
